27783ecec472191d2ca4ad52d80d17246203c504
Alexander <alexanderattar@gmail.com>
2025-02-11 16:50:40 -0500

    Fix the transfer ownership script

--- script/TransferOwnership.s.sol
+++ script/TransferOwnership.s.sol
@@ -3,30 +3,43 @@ pragma solidity 0.8.28;
 
 import "forge-std/Script.sol";
 import "../src/GAINS.sol";
+import "../src/MigrateHLGToGAINS.sol";
 
 /// @title TransferOwnership Script
-/// @notice Script to transfer ownership of the GAINS contract to a Gnosis Safe
+/// @notice Script to transfer ownership of the GAINS and MigrateHLGToGAINS contracts to a Gnosis Safe
 contract TransferOwnership is Script {
     /// @notice Executes the ownership transfer
-    /// @dev Reads GAINS_CONTRACT and GNOSIS_SAFE addresses from environment variables
+    /// @dev Reads GAINS_CONTRACT, MIGRATION_CONTRACT, GNOSIS_SAFE, and PRIVATE_KEY from environment variables
     function run() external {
         address GAINS_CONTRACT = vm.envAddress("GAINS_CONTRACT");
+        address MIGRATION_CONTRACT = vm.envAddress("MIGRATION_CONTRACT");
         address GNOSIS_SAFE = vm.envAddress("GNOSIS_SAFE");
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         require(GAINS_CONTRACT != address(0), "Invalid GAINS contract address");
+        require(MIGRATION_CONTRACT != address(0), "Invalid Migration contract address");
         require(GNOSIS_SAFE != address(0), "Invalid Gnosis Safe address");
 
         console.log("Starting ownership transfer...");
         console.log("GAINS Contract:", GAINS_CONTRACT);
+        console.log("Migration Contract:", MIGRATION_CONTRACT);
         console.log("New Owner (Gnosis Safe):", GNOSIS_SAFE);
 
-        vm.startBroadcast();
+        vm.startBroadcast(deployerPrivateKey);
 
+        // Transfer ownership of GAINS
         GAINS gains = GAINS(GAINS_CONTRACT);
-        address currentOwner = gains.owner();
-        console.log("Current owner:", currentOwner);
-
+        address currentGainsOwner = gains.owner();
+        console.log("Current GAINS owner:", currentGainsOwner);
         gains.transferOwnership(GNOSIS_SAFE);
+        console.log("GAINS ownership transferred to:", GNOSIS_SAFE);
+
+        // Transfer ownership of MigrateHLGToGAINS
+        MigrateHLGToGAINS migration = MigrateHLGToGAINS(MIGRATION_CONTRACT);
+        address currentMigrationOwner = migration.owner();
+        console.log("Current Migration owner:", currentMigrationOwner);
+        migration.transferOwnership(GNOSIS_SAFE);
+        console.log("Migration ownership transferred to:", GNOSIS_SAFE);
 
         vm.stopBroadcast();
 
